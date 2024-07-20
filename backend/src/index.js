d09d14de73a4330b1b45004f8a33c01dc2f1f391
Adam Stenzel <stenzeladam@gmail.com>
2024-07-20 00:09:39 -0700

    creating REST API

---
+++ backend/src/index.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+const express = require('express'); // Import Express.js framework
+const { createPool } = require('mysql2') // Destructuring createPool method from mysql2 module
+const app = express();
+app.use(express.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+}); // Start the server and log port info
+
+// create a MySQL connection pool
+// credentials in an .env file
+const pool = createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: 10
+});
+
+// Route for the root URL
+app.get('/', (req, res) => {
+    res.send('Cities of the World')
+});
+
+// Route to fetch cities from the database
+app.get('/api/cities', (req, res) => {
+    pool.query(
+        `SELECT 
+            world.city.ID AS CityID, 
+            world.city.Name AS CityName,
+            world.city.District,
+            world.city.Population AS CityPopulation,
+            world.country.Name AS CountryName,
+            world.country.Region
+         FROM world.city
+         JOIN world.country
+         ON world.city.CountryCode = world.country.Code
+         order by world.city.Population DESC;`, (err, citiesArray) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('500 Error: Internal Server Error');
+            return;
+        }
+        res.send(citiesArray); //send the fetched cities data as a response
+        console.log('200 OK');
+    });
+});
+
+app.use((req, res) => {
+    res.status(404).send('404 Error: Page Not Found');
+});
